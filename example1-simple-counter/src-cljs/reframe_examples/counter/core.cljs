b2278285ebae157f74465c8ab1ddb9c614fe14a6
Chris Oakman <chris.oakman@gmail.com>
2019-03-19 21:36:55 -0500

    counter example

--- example1-simple-counter/src-cljs/reframe_examples/counter/core.cljs
+++ example1-simple-counter/src-cljs/reframe_examples/counter/core.cljs
@@ -5,92 +5,71 @@
     [reagent.core :as reagent]
     [taoensso.timbre :as timbre]))
 
-;; -----------------------------------------------------------------------------
-;; Events
-
-(defn dispatch-timer-event
-  []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
-
-;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only one instance is ever
-;; created in the face of figwheel hot-reloading of this file.
-(defonce do-timer (js/setInterval dispatch-timer-event 1000))
+(def initial-state
+  {:num-times-clicked 0})
 
 ;; -----------------------------------------------------------------------------
-;; Subscriptions
-
-(rf/reg-sub
-  :time
-  (fn [db _]     ;; db is current app state. 2nd unused param is query vector
-    (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
-  :time-color
-  (fn [db _]
-    (:time-color db)))
-
-;; -----------------------------------------------------------------------------
-;; Event Handlers
-
-(rf/reg-event-db              ;; sets up initial application state
-  :initialize                 ;; usage:  (dispatch [:initialize])
-  (fn [_ _]                   ;; the two parameters are not important here, so use _
-    {:time (js/Date.)         ;; What it returns becomes the new application state
-     :time-color "#f88"}))    ;; so the application state will initially be a map with two keys
-
-
-(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
-  :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
-  (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-    (assoc db :time-color new-color-value)))   ;; compute and return the new application state
-
+;; Event Handlers: listen for events that were dispatched
+;; This is where you make changes to the app-db.
 
 (rf/reg-event-db
-  :timer
-  (fn [db [_ new-time]]
-    (assoc db :time new-time)))
+  :initialize
+  (fn [_db _event]
+    initial-state))
+
+(rf/reg-event-db
+  :increment
+  (fn [db _event]
+    (update-in db [:num-times-clicked] inc)))
+
+;; -----------------------------------------------------------------------------
+;; Subscriptions: get data out of app-db
+
+(rf/reg-sub
+  :click-count
+  (fn [db]
+    (:num-times-clicked db)))
 
 ;; -----------------------------------------------------------------------------
 ;; Components / Views
 
-(defn clock
+(defn BigCount
   []
-  [:div.example-clock
-   {:style {:color @(rf/subscribe [:time-color])}}
-   (-> @(rf/subscribe [:time])
-       .toTimeString
-       (str/split " ")
-       first)])
+  (let [current-count @(rf/subscribe [:click-count])]
+    [:div
+     {:style {:font-size "64px"}}
+     current-count]))
 
-(defn color-input
+(defn Buttons
   []
-  [:div.color-input
-   "Time color: "
-   [:input {:type "text"
-            :value @(rf/subscribe [:time-color])
-            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+  [:div.buttons
+    [:button.button.is-info.is-large
+      {:on-click #(rf/dispatch [:increment])}
+      "+1"]])
+    ; [:button.button.is-info.is-large
+    ;   {:on-click #(rf/dispatch [:decrement])}
+    ;   "-1"]])
 
-(defn ui
+(defn App
   []
-  [:div
-   [:h1 "Hello world, it is now"]
-   [clock]
-   [color-input]])
+  (let [current-count @(rf/subscribe [:click-count])]
+    [:section.section
+      [:div.container
+       [:h1.title.is-1 "re-frame counter: " current-count]
+       ; [BigCount]
+       [Buttons]]]))
 
 ;; -----------------------------------------------------------------------------
-;; Init / re-render
+;; Init + Render
+
+(defn- by-id [id]
+  (js/document.getElementById id))
+
+;; NOTE: this function is called by shadow-cljs on every hot module reload
+(defn render! []
+  (reagent/render [App] (by-id "app")))
 
 (defn init! []
   (rf/dispatch-sync [:initialize])
-  ;; begin rendering
-  (reagent/render [ui]
-                  (js/document.getElementById "app")))
-
-
-(defn re-render
-  "Forces a Reagent re-render of all components.
-   NOTE: this function is called after every shadow-cljs hot module reload"
-  []
-  (reagent/force-update-all))
+  ;; start rendering
+  (render!))
